477c2f6619621292d0c10617a61685fb4309b0cd
Manish Bohra <manish.bohra@sunarctechnologies.com>
2020-11-28 20:03:07 +0530

    initial commit

---
+++ bulk_sales_order_confirmation/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2020 Manish Kumar Bohra <manishbohra1994@gmail.com> or <manishkumarbohra@outlook.com>
+# License LGPL-3 - See http://www.gnu.org/licenses/Lgpl-3.0.html
+
+{
+    'name': 'Bulk Sales Order Confirmation',
+    'version': '1.0',
+    'summary': 'This app allows you to confirm multiple sales order in bulk',
+    'description': 'This app allows you to confirm multiple sales order in bulk',
+    'category': 'Sales',
+    'author': 'Manish Bohra',
+    'website': 'www.linkedin.com/in/manishkumarbohra',
+    'license': 'LGPL-3',
+    "data": [
+        'views/bulk_sales.xml',
+    ],
+    'images': ['static/description/bulk.gif'],
+    'depends': ['sale', 'sale_management'],
+    'installable': True,
+    'auto_install': False,
+}
